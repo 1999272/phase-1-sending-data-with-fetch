dd563d362472c6bc61dd8f59369c1e6d4a01e88a
Mark_Kamau <mark.wainaina@student.moringaschool.com>
2025-06-27 18:47:06 +0300

    Completed labs.

--- index.js
+++ index.js
@@ -1,1 +1,32 @@
-// Add your code here
+function submitData(name, email) {
+  const userData = {
+    name: name,
+    email: email
+  };
+
+  const configObj = {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+      "Accept": "application/json"
+    },
+    body: JSON.stringify(userData)
+  };
+
+  return fetch("http://localhost:3000/users", configObj)
+    .then(response => response.json())
+    .then(data => {
+      // Append new ID to the DOM
+      const body = document.querySelector("body");
+      const p = document.createElement("p");
+      p.textContent = `ID: ${data.id}`;
+      body.appendChild(p);
+    })
+    .catch(error => {
+      // Append error message to the DOM
+      const body = document.querySelector("body");
+      const errorMessage = document.createElement("p");
+      errorMessage.textContent = error.message;
+      body.appendChild(errorMessage);
+    });
+}
